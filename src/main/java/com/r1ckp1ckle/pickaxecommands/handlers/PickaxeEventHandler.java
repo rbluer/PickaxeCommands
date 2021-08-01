5b6d81c9b05d0206223ffda3843862304d9ddcc2
RoyalBlueRanger <665978+rbluer@users.noreply.github.com>
2021-08-01 17:06:04 -0400

    Setup the use of either { } or % % for the palceholder escape characters.  The admin can choose to use what they want. New placeholder {runAsPlayer} that will run the command as the player, instead of command sender. Created funtions to replace the various placeholders, or to check for the existance of the different placeholders.  These functions allows the use of either placeholder escape characters: { } or % %.

--- src/main/java/com/r1ckp1ckle/pickaxecommands/handlers/PickaxeEventHandler.java
+++ src/main/java/com/r1ckp1ckle/pickaxecommands/handlers/PickaxeEventHandler.java
@@ -3,6 +3,7 @@ package com.r1ckp1ckle.pickaxecommands.handlers;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -21,19 +22,61 @@ public class PickaxeEventHandler implements Listener {
         	HashMap<XMaterial, String> pPerms = PickaxeCommandsCore.getInstance().getCommandUtils().getPickaxePerms();
         	
         	String playerName = event.getPlayer().getName();
+        	CommandSender cmdRunner = Bukkit.getConsoleSender();
         	
         	XMaterial xMat = XMaterial.matchXMaterial( event.getMaterial() );
+        	String cmd = pCmds.get( xMat );
+        	
+        	// Replace any use of {player} placeholder in the command, with the player's name. 
+        	cmd = replacePlaceholder( "player", playerName, cmd );
+
+        	// Checks to see if the command should be ran as the player, or as console:
+        	if ( containsPlaceholder( "runAsPlayer", cmd ) ) {
+        		cmd = replacePlaceholder( "runAsPlayer", "", cmd );
+        		cmdRunner = event.getPlayer();
+        	}
+        	
         	
             if( pCmds.containsKey( xMat )) {
                 if( !pPerms.get( xMat ).equals("")) {
                     if( event.getPlayer().hasPermission( pPerms.get( xMat ) )) {
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), pCmds.get( xMat ).replace("%player%", playerName ));
+                        Bukkit.dispatchCommand( cmdRunner, cmd);
                     }
                 } 
                 else {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), pCmds.get( xMat ).replace("%player%", playerName));
+                    Bukkit.dispatchCommand( cmdRunner, cmd);
                 }
             }
         }
+    }
+    
+    /**
+     * <p>This takes a placeholder name, and replaces the placeholder with the targetValue.
+     * Either { } or % % can be used.  This allows for the flexibility within configs.
+     * </p>
+     * 
+     * @param placeholder
+     * @param targetValue
+     * @param command
+     * @return
+     */
+    private String replacePlaceholder( String placeholder, String targetValue, String command ) {
+    	return command
+    				.replace( "{" + placeholder + "}", targetValue )
+    				.replace( "%" + placeholder + "%", targetValue );
+    }
+    
+    /**
+     * <p>This uses a placeholder name, and checks if the placeholder exists within the
+     * command. Either { } or % % can be used.  This allows for the flexibility within configs.
+     * </p>
+     * 
+     * @param placeholder
+     * @param command
+     * @return
+     */
+    private boolean containsPlaceholder( String placeholder, String command ) {
+    	return command.contains( "{" + placeholder + "}" ) || 
+    		   command.contains( "%" + placeholder + "%" );
     }
 }
