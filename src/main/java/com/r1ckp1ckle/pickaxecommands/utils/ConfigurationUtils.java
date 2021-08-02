c79627fae918d989b79fb6a68d7f71e7236ef8ef
RoyalBlueRanger <665978+rbluer@users.noreply.github.com>
2021-08-01 20:12:57 -0400

    Hooking up reload realized that these functions are not using the configs that the main plugin is using, so eliminated the unused stuff to allow the real config to be used.

--- src/main/java/com/r1ckp1ckle/pickaxecommands/utils/ConfigurationUtils.java
+++ src/main/java/com/r1ckp1ckle/pickaxecommands/utils/ConfigurationUtils.java
@@ -3,16 +3,14 @@ package com.r1ckp1ckle.pickaxecommands.utils;
 import java.io.File;
 import java.util.logging.Level;
 
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-
 import com.r1ckp1ckle.pickaxecommands.PickaxeCommandsCore;
 
 public class ConfigurationUtils {
     private PickaxeCommandsCore pickaxeCommandsCore;
 
     private File configFile;
-    private FileConfiguration config;
+//    private FileConfiguration config;
+    
     public ConfigurationUtils(PickaxeCommandsCore pickaxeCommandsCore) {
         this.pickaxeCommandsCore = pickaxeCommandsCore;
         initConfig();
@@ -23,10 +21,15 @@ public class ConfigurationUtils {
             pickaxeCommandsCore.getDataFolder().mkdirs();
         }
         configFile = new File(pickaxeCommandsCore.getDataFolder(), "config.yml");
-        if (!configFile.exists()) pickaxeCommandsCore.saveResource("config.yml", false);
-        config = new YamlConfiguration();
+        
+        if (!configFile.exists()) {
+        	pickaxeCommandsCore.saveResource("config.yml", false);
+        }
+        
+//        config = new YamlConfiguration();
+        	
         try {
-            config.load(configFile);
+        	pickaxeCommandsCore.getConfig().load(configFile);
         } catch (Exception e) {
             pickaxeCommandsCore.getLogger().log(Level.SEVERE, "Failed to load plugin files.");
             pickaxeCommandsCore.getLogger().log(Level.SEVERE, "Error: " + e.getMessage());
@@ -35,23 +38,23 @@ public class ConfigurationUtils {
 
     public void reloadFiles() {
         try {
-            config.load(configFile);
+        	pickaxeCommandsCore.getConfig().load(configFile);
         } catch (Exception e) {
-            pickaxeCommandsCore.getLogger().log(Level.SEVERE, "Failed to load plugin files.");
+            pickaxeCommandsCore.getLogger().log(Level.SEVERE, "Failed to reload plugin files.");
             pickaxeCommandsCore.getLogger().log(Level.SEVERE, "Error: " + e.getMessage());
         }
     }
 
     public void saveFiles() {
         try {
-            config.save(configFile);
+        	pickaxeCommandsCore.getConfig().save(configFile);
         } catch (Exception e) {
             pickaxeCommandsCore.getLogger().log(Level.SEVERE, "Failed to save plugin files.");
             pickaxeCommandsCore.getLogger().log(Level.SEVERE, "Error: " + e.getMessage());
         }
     }
 
-    public FileConfiguration getConfig() {
-        return config;
-    }
+//    public FileConfiguration getConfig() {
+//        return config;
+//    }
 }
